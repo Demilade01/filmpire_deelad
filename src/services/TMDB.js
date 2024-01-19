e87788e2b16bda942b699b85e5418e5be9f1e6f1
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-01-19 12:37:20 -0800

    movie recommendations

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -34,7 +34,12 @@ export const tmdbApi = createApi({
     // Get Movie
     getMovie: builder.query({
       query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`
-    })
+    }),
+
+    // Get User Specific Lists
+    getRecommendations: builder.query({
+      query: ({movie_id, list}) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`
+    }),
   }),
 });
 
@@ -42,4 +47,5 @@ export const {
   useGetGenresQuery,
   useGetMoviesQuery,
   useGetMovieQuery,
+  useGetRecommendationsQuery,
 } = tmdbApi;
