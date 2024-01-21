7bf602248b7f011a9c28f79a024e62fc61d14044
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-01-21 14:35:10 -0800

    implement color mode

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { ColorModeContext } from '../../utils/ToggleColorMode'
 import { setUser, userSelector } from "../../features/auth";
 import useStyles from './styles';
 import Sidebar from '../Sidebar/Sidebar';
@@ -18,6 +19,8 @@ const NavBar = () => {
   const isMobile = useMediaQuery('(max-width:600px)');
   const theme = useTheme();
   const dispatch = useDispatch();
+
+  const colorMode = useContext(ColorModeContext);
 
   const token = localStorage.getItem('request_token');
   const sessionIdFromLocalStorage = localStorage.getItem('session_id');
@@ -38,7 +41,7 @@ const NavBar = () => {
         }
       }
     };
-    
+
     loginUser();
   }, [token]);
 
@@ -57,7 +60,7 @@ const NavBar = () => {
               <Menu />
             </IconButton>
           )}
-          <IconButton color="inherit" sx={{ ml: 1}} onClick={() => {}}>
+          <IconButton color="inherit" sx={{ ml: 1}} onClick={colorMode.toggleColorMode}>
             {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />  }
           </IconButton>
           {!isMobile && <Search />}
--- src/index.js
+++ src/index.js
@@ -4,23 +4,22 @@ import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 
+import ToggleColorModeProvider from './utils/ToggleColorMode';
 import App from './components/App';
 import store from './app/store';
 import './index.css';
-
-const theme = createTheme({});
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <Provider store={store}>
-  <ThemeProvider theme={theme}>
+  <ToggleColorModeProvider>
     <BrowserRouter>
       <StrictMode>
         <App />
       </StrictMode>,
     </BrowserRouter>,
-  </ThemeProvider>,
+  </ToggleColorModeProvider>,
   </Provider>,
 );
